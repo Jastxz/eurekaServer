fc3194898371ba804c39efc51ec6d7040fb0c097
Jastxz <javicraft14@gmail.com>
2026-01-11 20:06:47 +0100

    feat: Update Maven groupId and configure Eureka server to use localhost for its hostname and default service URL.

--- src/main/java/es/jastxz/eureka_server/EurekaServerApplication.java
+++ src/main/java/es/jastxz/eureka_server/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package org.javig.eureka_server;
+package es.jastxz.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
